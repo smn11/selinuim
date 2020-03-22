672bccd87b880e317f16bc126b1b089cf7f7ac21
smn11 <61623978+smn11@users.noreply.github.com>
2020-03-22 11:12:18 +0530

    Revert "add mohan"

--- se.java
+++ se.java
@@ -4,11 +4,9 @@ public class se {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hai");
-System.out.println("mohan");
-System.out.println("mohan");
-System.out.println("mohan")
 System.out.println("hai");
 System.out.println("hai");
+
 	}
 
 }
